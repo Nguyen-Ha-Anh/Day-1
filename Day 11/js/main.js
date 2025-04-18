c905d3f3c66e24476a904c720e4d7b2eea97c55d
Nguyen-Ha-Anh <anhnhf8@fullstack.edu.vn>
2025-04-18 23:03:30 +0700

    HW_Day11

---
+++ Day 11/js/main.js
@@ -0,0 +1,133 @@
+
+    /*document.writeln('<h1>hello anh em</h1>')
+
+    console.log('<h1>hello anh em</h1>')
+    console.log('1234', 54322)
+    console.log(1+2, 3-6, 3*6, -9/0)
+
+    console.log('hello' + ' anh em')
+    console.log('2'+ 2000)
+
+    // this is comment
+    /*
+    *this is comment
+    * hello anh em
+    * 
+    
+    /*
+    * Bien: variable
+    * use to store any temp value
+    * store to RAM
+    * reset -> remove
+    * declear a variable -> 4 ways
+    * var (<2015)
+    * let / const (>2015 - EC6)
+    * */
+
+    /*var a 
+    a = 1
+
+    //inital variable
+    var b = 1
+
+    // initial variable
+    var b = 1
+    let c = 1
+    const d = 1
+
+    a = a + 1
+    c = a + b
+    // d = d + 1
+    console.log(a, b)
+
+    /*
+    *oparator + - * /
+    *number + number = number
+    *number - number = number
+    *number * number = number
+    *number / number = number
+
+    *string + string = string
+    *string - string = NaN ( Not a number)
+    *string * string = NaN (NOt a number)
+    *string / string = NaN (NOt a number)
+
+    *String link number
+    *string + string = string
+    *string - string = number
+    *string * string = number
+    *string / string = number
+    
+    (*) JS try to convert your operator to corect operator -> should not depend on it 
+    * */
+
+    /* 
+    * a ** 2 = a * a
+    * a ** 3 = a * a * a
+    **/
+   // lấy dư: %
+//    console.log (10 % 2)
+   
+//    let a = 5
+    //  a ++
+    // //  ++ a
+    // const b = a + ++a + a++ + ++a + a-- - --a
+
+    // a = a + 5 // a += 5
+
+    // console.log(a++) 
+
+
+    /*
+    *comparison operator
+    * == / === (*)
+    *  >
+    *  <
+    * >=
+    * <=
+    * != / !==
+    * ? notsure
+    **/
+
+    /*
+    * data type
+    * string
+    * number
+    * bool
+    * null / undefine / unknow /
+    * object
+    * function
+    * 
+    * to check data type: typeof(<variable)
+    **/
+
+    // console.log(3===2)
+
+    // const a = undefined
+    // console.log(typeof a)
+
+    /*
+    * Concat: convert data type
+    *
+    * DataType(<variable)
+    * 
+    * string<-> number -> ok
+    * string -> bool
+    * length of string > 0 ----> true, else ->>> false
+    * 
+    * number -> bool
+    * 0 -> false else true
+    * 
+    * bool -> string
+    * true -> 'true'
+    * false -> 'false'
+    * 
+    * bool -> number
+    * true -> 1
+    * false -> 0
+    **/
+
+    // let là hàm chữ
+    let a = -1
+    a = Boolean(a)
+    console.log(a)
