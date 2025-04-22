60bf77e6f8692ff54851c6c298d09c1402d7640d
Nguyen-Ha-Anh <anhnhf8@fullstack.edu.vn>
2025-04-22 11:40:45 +0700

    HW_Day12

--- Day 11/js/main.js
+++ Day 11/js/main.js
@@ -128,6 +128,87 @@
     **/
 
     // let là hàm chữ
-    let a = -1
-    a = Boolean(a)
-    console.log(a)
+    // let a = -1
+    // a = Boolean(a)
+    // console.log(a)
+
+    // === <-> !==
+
+    // thêm ! để đảo ngược false thành true
+
+    // string rỗng => false
+
+    //if sẽ chạy đến khi tìm được code hợp lệ - chỉ 1 điều kiện hợp lệ
+
+    //case là chạy hết code, tìm ra nhiều hơn 1 điều kiện hợp lệ
+
+    // default chạy lại dòng code đó
+
+    // switch (a % 3) {
+    //     case 1:
+    //         console.log('du 1')
+    //         break
+    //     case 2:
+    //         console.log('du 2')
+    //         break
+    //     case 3:
+    //         console.log('du 3')
+    //         break
+    //     default:
+    //         console.log('hihi')
+        
+    // }
+
+    // const a = 5
+
+    // if (a % 2 === 0) console.log('a chan')
+    // else console.log('a le')
+
+    // chia 3 chu 0 -> du 0
+    // chia 3 chu 1 -> du 1
+    // chia 3 chu 1 -> du 3
+
+    // if (a % 3 === 0) console.log('du 0')
+    // else if (a % 3 === 1) console.log('du 1')
+    // else console.log('du 2')
+
+    // const a = 1, b = -2, c = 1
+    // delta = b**2 - 4*a*c
+
+    // if(delta < 0) console.log('vn')
+    //     else if (delta === 0) {
+    //     const x = -b / (2*a)
+    // }
+    // else {
+    //     x1 = (-b - delta**(1/2)/(2*a))
+    //     x2 = (-b + delta**(1/2)/(2*a))
+    //     console.log(x1, x2)
+    // }
+
+    /*
+    * Loop: re-use code
+    * for
+    *   for
+    *   for of
+    *   forEach
+    *   map
+    *   filter
+    *   reduce
+    * 
+    *   while
+    * 
+    * do while ( do not use)
+    **/
+
+    /*
+    * syntax
+    *
+    * for (init; condition; next step) {
+    * block
+    * }
+    **/
+
+    const a = 7
+    if (a < 1)
+        console.log(false)
+    for (let a > 1)
